d45caf0ca318916a5d11c203554654b62b84e52e
unknown <rizalbario3@gmail.com>
2024-06-10 12:32:46 +0700

    layout fragment_profile.xml from jason

--- app/src/main/java/lastsubmission/capstone/basantaraapps/interfaces/splashscreen/SplashActivity.kt
+++ app/src/main/java/lastsubmission/capstone/basantaraapps/interfaces/splashscreen/SplashActivity.kt
@@ -3,10 +3,13 @@ package lastsubmission.capstone.basantaraapps.interfaces.splashscreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import lastsubmission.capstone.basantaraapps.R
+import lastsubmission.capstone.basantaraapps.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
