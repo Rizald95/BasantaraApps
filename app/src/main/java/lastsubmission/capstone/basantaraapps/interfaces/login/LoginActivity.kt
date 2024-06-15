2d16e18f3fb3ec326b0c7d278eef36af768f9952
unknown <rizalbario3@gmail.com>
2024-06-16 05:18:59 +0700

    logic login, register done

--- app/src/main/java/lastsubmission/capstone/basantaraapps/interfaces/login/LoginActivity.kt
+++ app/src/main/java/lastsubmission/capstone/basantaraapps/interfaces/login/LoginActivity.kt
@@ -1,12 +1,20 @@
 package lastsubmission.capstone.basantaraapps.interfaces.login
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.fragment.app.commit
+import lastsubmission.capstone.basantaraapps.helper.Result
 import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AlertDialog
 import lastsubmission.capstone.basantaraapps.R
 import lastsubmission.capstone.basantaraapps.data.dummy.LoginData
 import lastsubmission.capstone.basantaraapps.databinding.ActivityLoginBinding
@@ -24,31 +32,93 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupView()
+        setupAction()
+        playAnimation()
 
-        // Terapkan animasi fade in pada komponen utama
-        val fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in_anim)
-        binding.tvLoginAccount.startAnimation(fadeInAnimation)
-        binding.welcome.startAnimation(fadeInAnimation)
-        binding.loginLayout.startAnimation(fadeInAnimation)
-        binding.tvSignup.startAnimation(fadeInAnimation)
-        binding.tvRegister.startAnimation(fadeInAnimation)
 
+        loginViewModel.loginAction.observe(this) { result ->
+            when (result) {
+                is Result.Loading -> {
+                    showLoading(true)
+                }
+                is Result.Success -> {
+                    showLoading(false)
+                    AlertDialog.Builder(this).apply {
+                        setTitle("Berhasil!")
+                        setMessage(R.string.message_login)
+                        setPositiveButton("Ke Halaman Utama") { _, _ ->
+                            replaceHomeFragment()
+                        }
+                        create()
+                        show()
+                    }
+                }
+                is Result.Error -> {
+                    toastFailed()
+                    showLoading(false)
+                }
+            }
+        }
+
+    }
+
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
         binding.btnLogin.setOnClickListener {
-            val email = binding.etEmail.text.toString().trim()
-            val password = binding.etPassword.text.toString().trim()
+            binding.apply {
+                val email = etEmail.text.toString().trim()
+                val password = etPassword.text.toString().trim()
 
-            if (validateLogin(email, password)) {
-                replaceHomeFragment()
-            } else {
-                Toast.makeText(this, "Email atau Password salah", Toast.LENGTH_SHORT).show()
+                if (email.isNotEmpty() && password.isNotEmpty()) {
+                    loginViewModel.login(email, password)
+                } else {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Email dan password harus diisi!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         }
 
         binding.tvRegister.setOnClickListener {
             navigateToRegister()
         }
-
     }
+
+    private fun playAnimation() {
+        val fadeInAnimation = ObjectAnimator.ofFloat(binding.tvLoginAccount, View.ALPHA, 1f).setDuration(200)
+        val welcomeAnimation = ObjectAnimator.ofFloat(binding.welcome, View.ALPHA, 1f).setDuration(200)
+        val layoutAnimation = ObjectAnimator.ofFloat(binding.loginLayout, View.ALPHA, 1f).setDuration(200)
+        val signUpAnimation = ObjectAnimator.ofFloat(binding.tvSignup, View.ALPHA, 1f).setDuration(200)
+        val registerAnimation = ObjectAnimator.ofFloat(binding.tvRegister, View.ALPHA, 1f).setDuration(200)
+
+        AnimatorSet().apply {
+            playSequentially(
+                fadeInAnimation,
+                welcomeAnimation,
+                layoutAnimation,
+                signUpAnimation,
+                registerAnimation
+            )
+            startDelay = 100
+        }.start()
+    }
+
 
     private fun navigateToRegister() {
         val intent = Intent(this, RegisterActivity::class.java)
@@ -59,17 +129,15 @@ class LoginActivity : AppCompatActivity() {
         supportFragmentManager.commit {
             replace(android.R.id.content, HomeFragment())
             setReorderingAllowed(true)
-            addToBackStack(null)  // Optional: This allows the user to press Back to return to the login screen
+            addToBackStack(null)
         }
     }
 
-    private fun validateLogin(email: String, password: String): Boolean {
-        val loginData = LoginData.getLoginData()
-        for (login in loginData) {
-            if (login.email == email && login.password == password) {
-                return true
-            }
-        }
-        return false
+    private fun toastFailed() {
+        Toast.makeText(this, R.string.failed_login, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
     }
 }
--- app/src/main/java/lastsubmission/capstone/basantaraapps/interfaces/register/RegisterActivity.kt
+++ app/src/main/java/lastsubmission/capstone/basantaraapps/interfaces/register/RegisterActivity.kt
@@ -1,17 +1,114 @@
 package lastsubmission.capstone.basantaraapps.interfaces.register
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import lastsubmission.capstone.basantaraapps.R
 import lastsubmission.capstone.basantaraapps.databinding.ActivityRegisterBinding
+import lastsubmission.capstone.basantaraapps.helper.Result
+import lastsubmission.capstone.basantaraapps.helper.ViewModelFactory
+import lastsubmission.capstone.basantaraapps.interfaces.login.LoginActivity
+import lastsubmission.capstone.basantaraapps.repository.UserRepository
+
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private val registerViewModel: RegisterViewModel by viewModels {
+        // Provide the ViewModelFactory if required
+        ViewModelFactory.getInstance(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupView()
+        setupListeners()
+        observeViewModel()
 
+
+    }
+
+    private fun setupListeners() {
+        binding.btnRegister.setOnClickListener {
+            val username = binding.etName.text.toString()
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (validateInput(username, email, password)) {
+                registerViewModel.register(username, email, password)
+            }
+        }
+    }
+
+    private fun observeViewModel() {
+        registerViewModel.registerUser.observe(this) { result ->
+            when (result) {
+                is Result.Loading -> {
+                    showLoading(true)
+                }
+                is Result.Success -> {
+                    AlertDialog.Builder(this).apply {
+                        setTitle("Selamat")
+                        setMessage("Akunmu sudah dibuat")
+                        setCancelable(false)
+                        setPositiveButton("Login"){_, _ ->
+                            val intent = Intent(context, LoginActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        create()
+                        show()
+                    }
+                }
+                is Result.Error -> {
+                    // Handle error
+                    Toast.makeText(this, "Registration failed: ${result.error}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun validateInput(username: String, email: String, password: String): Boolean {
+        return when {
+            username.isEmpty() -> {
+                binding.etName.error = "Please enter a username"
+                false
+            }
+            email.isEmpty() -> {
+                binding.etEmail.error = "Please enter an email"
+                false
+            }
+            password.isEmpty() -> {
+                binding.etPassword.error = "Please enter a password"
+                false
+            }
+            else -> true
+        }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
