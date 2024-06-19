d56967466b2d55d68c9feb9e86f77caec8bedfdc
Rizal <rizalbario3@gmail.com>
2024-06-19 20:50:43 +0700

    push basantara repository

--- app/src/main/java/lastsubmission/capstone/basantaraapps/interfaces/result/ResultActivity.kt
+++ app/src/main/java/lastsubmission/capstone/basantaraapps/interfaces/result/ResultActivity.kt
@@ -1,5 +1,7 @@
 package lastsubmission.capstone.basantaraapps.interfaces.result
 
+import android.net.Uri
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import lastsubmission.capstone.basantaraapps.R
@@ -7,6 +9,8 @@ import lastsubmission.capstone.basantaraapps.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResultBinding
+    private var currentImageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResultBinding.inflate(layoutInflater)
@@ -16,5 +20,33 @@ class ResultActivity : AppCompatActivity() {
             val bottomSheetFragment = BottomViewsFragment.newInstance("Deskripsi hasil penjelasan akan ditampilkan disini")
             bottomSheetFragment.show(supportFragmentManager,  BottomViewsFragment::class.java.simpleName)
         }
+
+        displayResult()
+    }
+
+    private fun displayResult() {
+        val imageUriString = intent.getStringExtra(EXTRA_IMAGE_URI)
+        val result = intent.getStringExtra(EXTRA_RESULT)
+
+        imageUriString?.let {uriString ->
+            val imageUri = Uri.parse(uriString)
+            Log.d("Image Uri", "showImage: $imageUri")
+
+            binding.placeholderImage.setImageURI(imageUri)
+        }
+
+        result?.let {
+            binding.scanResult.text = it
+
+        }
+    }
+
+
+
+
+
+    companion object {
+        const val EXTRA_IMAGE_URI = "extra_image_uri"
+        const val EXTRA_RESULT = "extra_result"
     }
 }
